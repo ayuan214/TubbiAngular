bbc7bd95c6d5a2e3d6eecb8bdaccff3478c22d5f
ayuan214 <alvinyuan@gmail.com>
2015-06-12 02:04:16 -0700

    feat: Marker Update
    - Fixed bug where had to double click item
    - Current location icon now green
    - Animation with marker

--- app/app5.js
+++ app/app5.js
@@ -8,6 +8,8 @@ formdata.lat_local = 38.537205;
 formdata.long_local = -121.752466;
 formdata.asr_results = "Home";
 formdata.language_local = "en-US";
+formdata.gps_bool = false; 
+//formdata.gps_first = 0;
 //formdata.asr_results = "台灣小吃";
 //formdata.language_local = "cmn-Hant-TW";
 var language_local = formdata.language_local;
@@ -25,31 +27,7 @@ app.filter('startFrom', function () {
 });
 
 app.controller('customersCrtl', function ($scope, $http, $timeout, geolocation) {
-    $http.post('ajax/getCustomers1.php', JSON.stringify(formdata)).success(function (data) {
-        $scope.list = data;
-        $scope.currentPage = 1; //current page
-        $scope.entryLimit = 5; //max no of items to display in a page
-        $scope.filteredItems = $scope.list.length; //Initially for no filter  
-        $scope.totalItems = $scope.list.length;
-        $scope.language_local = formdata.language_local;
-        $scope.lat = formdata.lat_local;
-        $scope.lon = formdata.long_local;
-        //console.log($scope.list)
-        $scope.initLoad = 0;
-        //initMap($scope.lat, $scope.lon);
-        resultMap($scope.list);
-        geolocation.getLocation().then(function(geo){
-            $scope.coords = {lat:geo.coords.latitude, long:geo.coords.longitude};
-            $scope.lat = $scope.coords.lat;
-            $scope.lon = $scope.coords.long;
-            formdata.lat_local = $scope.lat;
-            formdata.long_local = $scope.lon;
-            console.log($scope.lat);
-            console.log($scope.lon);
-            initMap($scope.lat, $scope.lon); 
-            $scope.loadData();
-        });
-    });
+
     //console.log($scope.map);
     $scope.setPage = function (pageNo) {
         $scope.currentPage = pageNo;
@@ -70,13 +48,35 @@ app.controller('customersCrtl', function ($scope, $http, $timeout, geolocation) 
             $scope.entryLimit = 5; //max no of items to display in a page
             $scope.filteredItems = $scope.list.length; //Initially for no filter  
             $scope.totalItems = $scope.list.length;
+            console.log(formdata);
             $scope.language_local = formdata.language_local;
+            //$scope.language_local = 'en-US';
+            $scope.lat = formdata.lat_local;
+            $scope.lon = formdata.long_local;
+            $scope.gps_bool = formdata.gps_bool; 
+            //$scope.gps_bool = formdata.gps_first; 
             //console.log($scope.list);
+ 
+            //reload the result map
+            $scope.initLoad = 0;
+            resultMap($scope.list, $scope.gps_bool, $scope.lat, $scope.lon);
         });
-        //reload the result map
-        $scope.initLoad = 0;
-        resultMap($scope.list);
-    }
+    };
+
+    $scope.loadData();
+    geolocation.getLocation().then(function(geo){
+        $scope.coords = {lat:geo.coords.latitude, long:geo.coords.longitude};
+        //$scope.coords = {lat:geo.coords.latitude, long:geo.coords.longitude};
+        //$scope.lat = $scope.coords.lat;
+        //$scope.lon = $scope.coords.long;
+        formdata.lat_local = $scope.coords.lat;
+        formdata.long_local = $scope.coords.long;
+        formdata.gps_bool = true; 
+        console.log($scope.lat);
+        console.log($scope.lon);
+        //initMap($scope.lat, $scope.lon); 
+        $scope.loadData();
+    });
 });
 
 app.directive('store', function () {
@@ -226,36 +226,92 @@ function handleNoGeolocation(errorFlag) {
         console.log(options);
     }
 // resultMap
-function resultMap(list) {
+function resultMap(list, gps_bool, lat_gps, lon_gps) {
 
     var map = new google.maps.Map(document.getElementById('map_result'), {
         zoom: 10,
         center: new google.maps.LatLng(list[0].Latitude, list[0].Longitude),
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    google.maps.event.trigger(map, 'resize');
+    //google.maps.event.trigger(map, 'resize');
 
     var infowindow = new google.maps.InfoWindow();
-    var marker, i;
+    var markers = [];
+    var bounds = new google.maps.LatLngBounds();
 
-    for (i = 0; i < list.length; i++) {
-        marker = new google.maps.Marker({
+    for (var i = 0; i < list.length; i++) {
+        markers[i] = new google.maps.Marker({
             position: new google.maps.LatLng(list[i].Latitude, list[i].Longitude),
-            map: map
+            map: map,
+            animation: google.maps.Animation.DROP
+
         });
-        google.maps.event.addListener(marker, 'mouseover', (function (marker, i) {
+        google.maps.event.addListener(markers[i], 'mouseover', (function (marker, j) {
             return function () {
-                infowindow.setContent(list[i].Name);
+                infowindow.setContent(list[j].Name);
                 infowindow.open(map, marker);
             }
-        })(marker, i));
+        })(markers[i], i));
+
+        bounds.extend(markers[i].getPosition());
     }
+    console.log(markers);
     map.setCenter(new google.maps.LatLng(list[0].Latitude, list[0].Longitude));
-    //map.setZoom(map.getZoon());
-    /*google.maps.event.addListener(map, 'center_changed', function () {
-        // Whatever.
-        console.log('centerchanged');
-    });*/
+    map.fitBounds(bounds);
+     if(map.getZoom()> 15){
+        map.setZoom(15);
+    };
+
+    if (gps_bool = true) {
+        marker = new google.maps.Marker({
+            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            position: new google.maps.LatLng(lat_gps, lon_gps),
+            map: map
+        });
+
+        markers.push(marker);
+        bounds.extend(marker.getPosition());
+
+
+        //map.setCenter(new google.maps.LatLng(lat_gps, lon_gps));
+        map.fitBounds(bounds);
+        if(map.getZoom()> 15){
+            map.setZoom(15);
+        };
+    };
+
+    var a = 0;
+
+        //center the map to a specific spot (city)
+        //map.setCenter(center); 
+
+        //center the map to the geometric center of all markers
+        //map.setCenter(new google.maps.Latlng(lat_gps,lon_gps));
+
+        //map.fitBounds(bounds);
+
+        //remove one zoom level to ensure no marker is on the edge.
+       // map.setZoom(map.getZoom()-1); 
+
+        // set a minimum zoom 
+        // if you got only 1 marker or all markers are on the same address map will be zoomed too much.
+       // if(map.getZoom()> 15){
+        //  map.setZoom(15);
+        //};
+
+
+    console.log(markers)
+
+
+    /*
+    if (center_first == true) {
+        map.setCenter(new google.maps.LatLng(list[0].Latitude, list[0].Longitude));
+        center_first == false;
+        return center_first; 
+    }
+    else {
+        map.setCenter(new google.maps.LatLng(list[0].Latitude, list[0].Longitude));
+    }; */
 };
 
 /*************** Speech script executes start here... ***********
